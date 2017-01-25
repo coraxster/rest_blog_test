6f5e0b018edeb6db6a584e58b61f39c64b2612d5
dmitry kuzmin <corax@list.ru>
2017-01-25 14:10:04 +0300

    PostController

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+class PostController extends Controller
+{
+    public function listPosts(Request $request){
+    	return Post::all();
+    }
+}

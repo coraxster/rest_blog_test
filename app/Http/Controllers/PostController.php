0dc5bb93f9c9dbe7e7f2087315bf06ea22d5092c
dmitry kuzmin <corax@list.ru>
2017-01-25 15:58:32 +0300

    Post Controller, @List @Add @Delete @Update

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,6 +14,68 @@ class PostController extends Controller
     }
 
     public function listPosts(Request $request){
-    	return Auth::user()->posts()->get();
+    	$this->validate($request, [
+		    'On_page' => 'integer|min:1',
+		    'Order_by' => 'in:id,title,created_at',
+		    'Order_direction' => 'in:asc,desc',
+		    'Post_type' => 'in:post,draft'
+		]);
+
+    	$scope = Auth::user()->posts();
+
+    	if ($request->has('On_page')){
+    		$scope->take($request->On_page);
+    	}
+
+    	if ($request->has('Order_by')){
+    		if ($request->has('Order_direction')){
+	    		$scope->orderBy($request->Order_by, $request->Order_direction);
+	    	}else{
+	    		$scope->orderBy($request->Order_by);
+	    	}
+    	}
+
+    	if ($request->has('Post_type')){
+    		$scope = $scope->PostType($request->Post_type);
+    	}
+
+    	return $scope->get();
     }
+
+    public function addPost(Request $request){
+    	$this->validate($request, [
+    		'props_json' => 'json',
+    		'created_at' => 'date',
+    		'post_type' => 'in:post,draft', 
+		]);
+    	return Auth::user()->posts()->create($request->toArray());
+    }
+
+    public function editPost(Request $request){
+    	$this->validate($request, [
+    		'props_json' => 'json',
+    		'created_at' => 'date',
+    		'post_type' => 'in:post,draft', 
+    		'post_id' => 'exists:Items,item_id'
+		]);
+		$post = Auth::user()->posts()->where(['item_id' => $request->post_id])->firstOrFail();
+		if ($post->update($request->toArray())){
+			return $post;
+		}else{
+			return 'error';
+		}
+    }
+
+    public function deletePost(Request $request){
+    	$post = Auth::user()->posts()->where(['item_id' => $request->post_id])->firstOrFail();
+    	if ($post->delete()){
+    		return 'ok';
+    	}else{
+    		return 'error';
+    	}
+    }
+
+
+
+
 }

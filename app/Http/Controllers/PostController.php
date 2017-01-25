b20467e4a7da02b6bb2f469e48ef60565ba43b62
dmitry kuzmin <corax@list.ru>
2017-01-25 14:37:54 +0300

    User Scope for posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
+	public function __construct()
+    {
+        $this->middleware('token_auth');
+    }
+
     public function listPosts(Request $request){
-    	return Post::all();
+    	return Auth::user()->posts()->get();
     }
 }

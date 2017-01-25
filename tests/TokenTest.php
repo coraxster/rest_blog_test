075543e98dfc6e3b5d740b076ae8aeff22eda691
dmitry kuzmin <corax@list.ru>
2017-01-25 12:32:06 +0300

    test GetToken

---
+++ tests/TokenTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class TokenTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testGetToken()
+    {
+    	$faker = Faker\Factory::create();
+    	$userData = [
+    		'email' => $faker->unique()->safeEmail,
+    		'password' => 'password' //$faker->unique()->password()
+    	];
+        $user = factory(\App\User::class)
+        	->create([
+        		'email' => $userData['email'],
+        		'password' => bcrypt($userData['password'])
+        	]);
+        $response = $this->call('POST', '/json/auth', [
+        		'email' => $userData['email'],
+	    		'password' => $userData['password']
+        	]);
+        $this->seeInDatabase('users', [
+                'email' => $userData['email'],
+	    		'token' => $response->original
+            ]);
+
+    }
+}
